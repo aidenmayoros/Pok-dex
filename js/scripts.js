c4166b9e9bdb48c87ab8cef0b2df43b0f6b1e938
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-03-30 14:03:22 -0700

    1.3 task submission with added styling

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,22 @@ let pokemonList = [
 		types: ['water'],
 	},
 ];
+
+// Returns the Pokemon object that has the highest height
+const tallestPokemon = pokemonList.reduce(function (prev, current) {
+	return prev.height > current.height ? prev : current;
+});
+
+// Loop through the Pokemon and print them to webpage in a Div, adding some extra text to the tallest one.
+for (let i = 0; i < pokemonList.length; i++) {
+	let pokemon = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
+	if (pokemonList[i] === tallestPokemon) {
+		document.write("<div class='pokemon'>");
+		document.write(pokemon + ' <= This is the tallest Pokemon!');
+		document.write('</div>');
+	} else {
+		document.write("<div class='pokemon'>");
+		document.write(pokemon);
+		document.write('</div>');
+	}
+}
