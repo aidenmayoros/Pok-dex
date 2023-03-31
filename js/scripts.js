072535f9acc59bbb18b85995edaa93378f0f41b6
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-03-31 13:03:43 -0700

    Adjusted data to be inside of a IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,43 @@
-let pokemonList = [
-	{
-		name: 'Bulbasaur',
-		height: 0.7,
-		types: ['grass', 'poison'],
-	},
-	{
-		name: 'Charmander',
-		height: 0.6,
-		types: ['fire'],
-	},
-	{
-		name: 'Squirtle',
-		height: 0.5,
-		types: ['water'],
-	},
-];
+let pokemonRepository = (function () {
+	let pokemonList = [
+		{
+			name: 'Bulbasaur',
+			height: 0.7,
+			types: ['grass', 'poison'],
+		},
+		{
+			name: 'Charmander',
+			height: 0.6,
+			types: ['fire'],
+		},
+		{
+			name: 'Squirtle',
+			height: 0.5,
+			types: ['water'],
+		},
+	];
 
-// Returns the Pokemon object that has the highest height
-const tallestPokemon = pokemonList.reduce(function (prev, current) {
+	function getAll() {
+		return pokemonList;
+	}
+
+	function add(newPokemon) {
+		pokemonList.push(newPokemon);
+	}
+
+	return {
+		getAll: getAll,
+		add: add,
+	};
+})();
+
+// Returns the Pokemon object that has the highest height from the Pokemon Repository.
+const tallestPokemon = pokemonRepository.getAll().reduce(function (prev, current) {
 	return prev.height > current.height ? prev : current;
 });
 
-// Loop through the Pokemon and print them to webpage in a Div, adding some extra text to the tallest one.
-pokemonList.forEach((pokemon) => {
+// Loop through all the Pokemon and print them to the webpage and say which one is the tallest one.
+pokemonRepository.getAll().forEach((pokemon) => {
 	let pokemonString = `${pokemon.name} (height: ${pokemon.height})`;
 	if (pokemon === tallestPokemon) {
 		document.write("<p class='pokemon'>" + pokemonString + ' <= This is the tallest Pokemon! </p>');
